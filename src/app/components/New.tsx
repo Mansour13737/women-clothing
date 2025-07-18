1bd8080825b738128e8ca409601a3dfcb19c66eb
Mansour13737 <mansournosrati69@gmail.com>
2025-07-19 03:26:15 +0330

    6

--- src/app/components/New.tsx
+++ src/app/components/New.tsx
@@ -10,9 +10,11 @@ const img = [
 
 export default function New() {
   return (
-    <div className="w-full h-[25.4%] flex justify-around">
-      {img.map((item)=> (
-        <Image src ={item.src} width={300} height={1400} alt="Pic"/>
+    <div className="w-full h-[25.4%] flex justify-around px-1">
+      {img.map((item,index)=> (
+        <div key={index}>
+          <Image src ={item.src} width={300} height={1000} alt="Pic" className="h-43 w-32"/>
+        </div>
       ))}
     </div>
   )
